207aa96f9e2db0535a6cdac0ea688d31985ece35
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2021-02-12 17:19:43 +0900

    [1]Two Sum

---
+++ editor/en/[1]Two Sum.js
@@ -0,0 +1,67 @@
+//Given an array of integers nums and an integer target, return indices of the t
+//wo numbers such that they add up to target.
+//
+// You may assume that each input would have exactly one solution, and you may n
+//ot use the same element twice.
+//
+// You can return the answer in any order.
+//
+//
+// Example 1:
+//
+//
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+//
+//
+// Example 2:
+//
+//
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+//
+//
+// Example 3:
+//
+//
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+//
+//
+//
+// Constraints:
+//
+//
+// 2 <= nums.length <= 103
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+//
+// Related Topics Array Hash Table
+// 👍 19235 👎 684
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let result;
+
+  nums.forEach((num_a, i) => {
+    nums.forEach((num_b, j) => {
+      const sameItem = i === j;
+      if (!sameItem) {
+        const sum = num_a + num_b;
+        if (sum === target) {
+          result = [i, j];
+        }
+      }
+    });
+  });
+
+  return result;
+};
+//leetcode submit region end(Prohibit modification and deletion)
