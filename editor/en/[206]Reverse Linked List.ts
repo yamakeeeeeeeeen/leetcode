3737a97aaa69a137feb47f445a2749e4318d81c6
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2021-02-24 22:28:50 +0900

    [206]Reverse Linked List

---
+++ editor/en/[206]Reverse Linked List.ts
@@ -0,0 +1,73 @@
+//Given the head of a singly linked list, reverse the list, and return the rever
+//sed list.
+//
+//
+// Example 1:
+//
+//
+//Input: head = [1,2,3,4,5]
+//Output: [5,4,3,2,1]
+//
+//
+// Example 2:
+//
+//
+//Input: head = [1,2]
+//Output: [2,1]
+//
+//
+// Example 3:
+//
+//
+//Input: head = []
+//Output: []
+//
+//
+//
+// Constraints:
+//
+//
+// The number of nodes in the list is the range [0, 5000].
+// -5000 <= Node.val <= 5000
+//
+//
+//
+// Follow up: A linked list can be reversed either iteratively or recursively. C
+//ould you implement both?
+// Related Topics Linked List
+// 👍 6391 👎 123
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  if (!head) return null;
+
+  const tmp_list: number[] = [];
+  while (head) {
+    tmp_list.push(head.val);
+    head = head.next;
+  }
+
+  let list = new ListNode(-1, null);
+  const dummy = list;
+  const rev_list = tmp_list.reverse();
+
+  for (const item of rev_list) {
+    list.next = new ListNode(item);
+    list = list.next;
+  }
+
+  return dummy.next;
+}
+//leetcode submit region end(Prohibit modification and deletion)
