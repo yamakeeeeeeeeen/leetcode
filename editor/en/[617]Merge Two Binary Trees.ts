c48c9898608d9d870ed5fcf7403517e05038459d
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2021-04-17 23:48:01 +0900

    [617]Merge Two Binary Trees

---
+++ editor/en/[617]Merge Two Binary Trees.ts
@@ -0,0 +1,70 @@
+//You are given two binary trees root1 and root2.
+//
+// Imagine that when you put one of them to cover the other, some nodes of the t
+//wo trees are overlapped while the others are not. You need to merge the two tree
+//s into a new binary tree. The merge rule is that if two nodes overlap, then sum
+//node values up as the new value of the merged node. Otherwise, the NOT null node
+// will be used as the node of the new tree.
+//
+// Return the merged tree.
+//
+// Note: The merging process must start from the root nodes of both trees.
+//
+//
+// Example 1:
+//
+//
+//Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+//Output: [3,4,5,5,4,null,7]
+//
+//
+// Example 2:
+//
+//
+//Input: root1 = [1], root2 = [1,2]
+//Output: [2,2]
+//
+//
+//
+// Constraints:
+//
+//
+// The number of nodes in both trees is in the range [0, 2000].
+// -104 <= Node.val <= 104
+//
+// Related Topics Tree
+// 👍 4209 👎 194
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function mergeTrees(
+  root1: TreeNode | null,
+  root2: TreeNode | null
+): TreeNode | null {
+  const merge = (n1: TreeNode | null, n2: TreeNode | null): TreeNode | null => {
+    if (!n1) return n2;
+    if (!n2) return n1;
+
+    n1.val += n2.val;
+    n1.left = merge(n1.left, n2.left);
+    n1.right = merge(n1.right, n2.right);
+
+    return n1;
+  };
+
+  return merge(root1, root2);
+}
+//leetcode submit region end(Prohibit modification and deletion)
